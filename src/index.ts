9e205c2a6120ab4e91e5a4923b6d15c805ecadfd
chabuuuu <haphuthinh332004@gmail.com>
2024-01-26 17:05:13 +0700

    delete admin and add example

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from 'express'
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 const swaggerDocument = YAML.load('./swagger.yaml');
